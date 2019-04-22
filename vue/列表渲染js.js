37b8965204ea3bfa0b7ad1a9bbeb6ff9e3a62038
opendeng <315780202@qq.com>
2019-04-22 16:33:03 +0800

    笔记

---
+++ vue/列表渲染js.js
@@ -0,0 +1,25 @@
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+      items: [
+        {message: 'foo' },
+        {message: 'Bar' },
+        {message: 'Bar' },
+        {message: 'Bar' },
+        {message: 'Bar' },
+        {msg: '123456' },
+        {msg: '123456' },
+        {msg: '123456' },
+        {msg: '123456' },
+        {msg: '123456' },
+        {msg: '123456' },
+        {msg: '123456' },
+        
+      ],
+      object: {
+        FirstName: 'John',
+        LastName: 'Doe',
+        Age: 30
+      }
+    }
+  })
