8b571d2418cd12d7a64b68176cde6f4fa818a742
opendeng <315780202@qq.com>
2019-04-22 17:35:25 +0800

    笔记

---
+++ vue/事件处理器js.js
@@ -0,0 +1,17 @@
+var vm =new Vue({
+el:"#aaa",
+data: {
+    counter: 0
+  },
+  //方法
+  methods: {
+    greet: function (event) {
+      // `this` 在方法里指当前 Vue 实例
+      alert('Hello ' + this.name + '!')
+      // `event` 是原生 DOM 事件
+      alert(event.target.tagName)
+    }
+  }
+
+
+})
--- vue/列表渲染js.js
+++ vue/列表渲染js.js
@@ -16,10 +16,18 @@ var example1 = new Vue({
         {msg: '123456' },
         
       ],
+      numbers: [ 1, 2, 3, 4, 5 ],
       object: {
         FirstName: 'John',
         LastName: 'Doe',
         Age: 30
+      }
+    },
+    computed: {
+      evenNumbers: function () {
+        return this.numbers.filter(function (number) {
+          return number % 2 === 0
+        })
       }
     }
   })
